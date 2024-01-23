60a69bc3947fb2906c665a979fa0bdf60dc6a2a3
Dave Alden <dpa99c@gmail.com>
2024-01-23 07:46:42 +0000

    WIP

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,33 +1,30 @@
 export interface GeminiXPluginPlugin {
-  initModel(options: { params: ModelParams }): Promise<void>;
-
-  sendMessage(options: { text: string }): Promise<{ result: string }>;
-  countTokens(options: { text: string }): Promise<{ result: number }>;
+  initModel(options: { params:ModelParams }): Promise<void>;
+  sendMessage(userInputText:string, options?: { streamResponse?:boolean, imageUris?:string[] }): Promise<{ responseText:string, isFinal:boolean }>;
+  countTokens(userInputText:string, options?: { imageUris?:string[] }): Promise<{ count:number }>;
+  initChat(chatHistory?:ChatHistoryItem[]): Promise<void>;
+  sendChatMessage(userInputText:string, options?: { streamResponse?:boolean, imageUris?:string[] }): Promise<{ responseText:string, isFinal:boolean }>;
+  countChatTokens(options?: { text?:string, imageUris?:string[] }): Promise<{ count:number }>;
+  getChatHistory(): Promise<{ chatHistory:ModelChatHistoryItem[] }>;
 }
 
 export interface ModelParams {
-  modelName: string;
-  apiKey: string;
-  temperature?: number;
-  topK?: number;
-  topP?: number;
-  maxOutputTokens?: number;
-  stopSequences?: string[];
-  safetySettings?: SafetySettings;
+  modelName:string;
+  apiKey:string;
+  temperature?:number;
+  topK?:number;
+  topP?:number;
+  maxOutputTokens?:number;
+  stopSequences?:string[];
+  safetySettings?:SafetySettings;
 }
 
 export interface SafetySettings {
-  [SafetySettingHarmCategory.DEROGATORY]?: SafetySettingLevel;
-  [SafetySettingHarmCategory.TOXICITY]?: SafetySettingLevel;
-  [SafetySettingHarmCategory.VIOLENCE]?: SafetySettingLevel;
-  [SafetySettingHarmCategory.SEXUAL]?: SafetySettingLevel;
-  [SafetySettingHarmCategory.MEDICAL]?: SafetySettingLevel;
-  [SafetySettingHarmCategory.DANGEROUS]?: SafetySettingLevel;
-  [SafetySettingHarmCategory.HARASSMENT]?: SafetySettingLevel;
-  [SafetySettingHarmCategory.HATE_SPEECH]?: SafetySettingLevel;
-  [SafetySettingHarmCategory.SEXUALLY_EXPLICIT]?: SafetySettingLevel;
-  [SafetySettingHarmCategory.DANGEROUS_CONTENT]?: SafetySettingLevel;
-  [SafetySettingHarmCategory.UNSPECIFIED]?: SafetySettingLevel;
+  [SafetySettingHarmCategory.HARASSMENT]?:SafetySettingLevel;
+  [SafetySettingHarmCategory.HATE_SPEECH]?:SafetySettingLevel;
+  [SafetySettingHarmCategory.SEXUALLY_EXPLICIT]?:SafetySettingLevel;
+  [SafetySettingHarmCategory.DANGEROUS_CONTENT]?:SafetySettingLevel;
+  [SafetySettingHarmCategory.UNSPECIFIED]?:SafetySettingLevel;
 }
 
 export enum SafetySettingLevel{
@@ -39,15 +36,25 @@ export enum SafetySettingLevel{
 }
 
 export enum SafetySettingHarmCategory{
-  DEROGATORY = "DEROGATORY",
-  TOXICITY = "TOXICITY",
-  VIOLENCE = "VIOLENCE",
-  SEXUAL = "SEXUAL",
-  MEDICAL = "MEDICAL",
-  DANGEROUS = "DANGEROUS",
   HARASSMENT = "HARASSMENT",
   HATE_SPEECH = "HATE_SPEECH",
   SEXUALLY_EXPLICIT = "SEXUALLY_EXPLICIT",
   DANGEROUS_CONTENT = "DANGEROUS_CONTENT",
   UNSPECIFIED = "UNSPECIFIED"
+}
+
+export interface ChatHistoryItem {
+  isUser:boolean;
+  text?:string;
+  imageUris?:string[];
+}
+
+export interface ModelChatHistoryPart{
+  type:string;
+  content:string;
+}
+
+export interface ModelChatHistoryItem {
+  isUser:boolean;
+  parts:ModelChatHistoryPart[];
 }
