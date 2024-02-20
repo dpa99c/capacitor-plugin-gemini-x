a6b159020d52208aa5da32e861e19f828983f1b0
Dave Alden <dave@workingedge.co.uk>
2024-02-20 22:17:44 +0000

    WIP

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "CapacitorGeminiXPlugin",
+    name: "CapacitorGeminiX",
     platforms: [.iOS(.v13)],
     products: [
         .library(
