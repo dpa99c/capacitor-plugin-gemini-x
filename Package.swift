be301c66368d05c1d66bb47f715ef7d3dcfd49d7
Dave Alden <dpa99c@gmail.com>
2024-02-20 19:40:37 +0000

    WIP

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "CapacitorGeminiXPlugin",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "GeminiXPlugin",
+            targets: ["GeminiXPlugin"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/google/generative-ai-swift.git", branch: "main")
+    ],
+    targets: [
+        .target(
+            name: "GeminiXPlugin",
+            dependencies: [
+                .product(name: "Capacitor", package: "generative-ai-swift"),
+                .product(name: "Cordova", package: "generative-ai-swift")
+            ],
+            path: "ios/Sources/GeminiXPlugin"),
+        .testTarget(
+            name: "GeminiXPluginTests",
+            dependencies: ["GeminiXPlugin"],
+            path: "ios/Tests/GeminiXPluginTests")
+    ]
+)
---
+++ ios/Sources/GeminiXPlugin/lib/README.md
@@ -0,0 +1,10 @@
+This repo contains the native iOS implementation of the [GeminiX abstraction layer](https://github.com/dpa99c/gemini-x) for using Google's Gemini AI SDK in cross-platform mobile apps on iOS, Android and Web platforms.
+
+It is written in Swift 5.0 and wraps the [Gemini SDK for Swift](https://ai.google.dev/tutorials/swift_quickstart).
+
+It is included as a Git submodule by:
+ - [Cordova GeminiX plugin](https://github.com/dpa99c/cordova-plugin-gemini-x)
+ - [Capacitor GeminiX plugin](https://github.com/dpa99c/capacitor-plugin-gemini-x) - coming soon
+ - [React Native GeminiX module](https://github.com/dpa99c/react-native-gemini-x) - coming soon
+ - [Flutter GeminiX module](https://github.com/dpa99c/flutter-gemini-x) - coming soon
+
